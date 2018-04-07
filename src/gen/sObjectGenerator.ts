c22f9cad3378d9ff8dd732ba93bf19e4fb829fdc
Charles Jonas <charlie@callawaycloudconsulting.com>
2018-04-07 16:28:32 -0600

    including salesforce label in generation

--- src/gen/sObjectGenerator.ts
+++ src/gen/sObjectGenerator.ts
@@ -397,7 +397,7 @@ export class SObjectGenerator {
         return {
             name: `sField`,
             arguments: [
-                `{apiName: '${decoratorProps.apiName}', readOnly: ${decoratorProps.readOnly}, required: ${decoratorProps.required}, reference:${ref}, childRelationship: ${decoratorProps.childRelationship}, salesforceType: ${sfType}}`
+                `{apiName: '${decoratorProps.apiName}', readOnly: ${decoratorProps.readOnly}, required: ${decoratorProps.required}, reference:${ref}, childRelationship: ${decoratorProps.childRelationship}, salesforceType: ${sfType}, salesforceLabel: '${decoratorProps.salesforceLabel}'}`
             ]
         };
     }
