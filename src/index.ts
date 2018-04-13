3e18b97605f171a163024ad0ca82528174a86e21
Charles Jonas <charlie@callawaycloudconsulting.com>
2018-04-13 12:21:44 -0600

    removing pointless folders

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-export * from './main/lib/sObject';
-export * from './main/lib/rest';
-export * from './auth/baseConfig';
+export * from './lib/sObject';
+export * from './lib/rest';
+export { setDefaultConfig, BaseConfig } from './auth/baseConfig';
 export * from './auth/oauth';
-export * from './main/lib/sObjectDecorators';
-export * from './main/lib/sObjectDescribe';
-export * from './main/lib/composite';
-export * from './main/lib/queryHelper';
-export * from './main/lib/compositeFieldTypes';
+export * from './lib/sObjectDecorators';
+export * from './lib/sObjectDescribe';
+export * from './lib/composite';
+export * from './lib/queryHelper';
+export * from './lib/compositeFieldTypes';
