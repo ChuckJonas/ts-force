a9562ced0eaae3637232d23f6767d520a7c75920
Charlie Jonas <charlie@callawaycloudconsulting.com>
2021-01-04 13:54:13 -0700

    updating readme

--- ts-force/src/auth/oAuth2.ts
+++ ts-force/src/auth/oAuth2.ts
@@ -214,6 +214,7 @@ export interface UsernamePasswordTokenFlow extends BaseTokenRequest {
 }
 
 export interface JWTTokenRequest {
+  instanceUrl: string;
 
   grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer';
 
